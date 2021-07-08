577c21fbf7daffc2fb00c21322993641cd387331
201851019-iiitv <201851019@iiitvadodara.ac.in>
2021-07-08 14:38:20 +0530

    added format

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 
-const API_URL = "http://localhost:3000/api/posts";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "https://my-blogging.herokuapp.com/api/posts/";
+const API_BASE_URL = "https://my-blogging.herokuapp.com/";
 
 window.onload = () => {
     getPosts();
